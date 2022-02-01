42eac5f79c906ebe7e2a3781469312699785e501
Brian Zhou <44906282+br-zhou@users.noreply.github.com>
2022-01-31 17:20:26 -0800

    Update player_movement.cs

--- week-1/player_movement.cs
+++ week-1/player_movement.cs
@@ -1,1 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class player_movement : MonoBehaviour
+{
+    const float speed = 10f;
+    const float jumpVelocity = 10f;
+    Rigidbody2D rb;
+    float inputX;
+
+    [SerializeField] LayerMask groundLayers;
+    [SerializeField] Transform groundCheckTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
+
+        if(Input.GetButton("Jump") && GroundCheck())
+		{
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+		}
+    }
+
+    bool GroundCheck()
+	{
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckTrans.position, 0.1f, groundLayers);
+
+        if(colliders.Length > 0)
+		{
+            return true;
+		}
+        else
+		{
+            return false;
+		}
+	}
+}
